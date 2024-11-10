2a34223fd3bb910b29d4365c75a4995edfb14d1d
filozopdasha <dariafilozop@gmail.com>
2024-11-10 14:41:33 +0200

    done conversions

--- rustlings/exercises/conversions/from_into.rs
+++ rustlings/exercises/conversions/from_into.rs
@@ -33,12 +33,38 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
+
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.is_empty() {
+            return Person::default();
+        }
+
+        let parts: Vec<&str> = s.split(',').collect();
+
+        if parts.len() < 2 {
+            return Person::default();
+        }
+
+        let name = parts[0].trim();
+        if name.is_empty() {
+            return Person::default();
+        }
+        let age_str = parts[1].trim();
+        let age = age_str.parse::<usize>();
+
+        if age.is_err() || parts.len() > 2 {
+            return Person::default();
+        }
+
+        match age {
+            Ok(age) => Person { name: name.to_string(), age },
+            Err(_) => Person::default(),
+        }
     }
 }
+
 
 fn main() {
     // Use the `from` function
